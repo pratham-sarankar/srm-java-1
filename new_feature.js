3d946a62f37e2d10c415e6a19cbd9645d6353177
naveen <naveenkallepalli46@gmail.com>
2026-04-07 17:00:19 +0530

    Intial commit

--- new_feature.js
+++ new_feature.js
@@ -1,5 +1,8 @@
-function print() {
-    console.log("Hello World");
+function print(){
+    console.log("Hello, World!");
 }
-
 print();
+function printHello(){
+    console.log("Hello, World!");
+}
+printHello();
